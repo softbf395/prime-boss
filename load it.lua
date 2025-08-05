c81271734bdff42363bd5a8920ccdf4e14308379
softbf395 <130502864+softbf395@users.noreply.github.com>
2025-08-05 17:56:58 -0500

    Update load it.lua

--- load it.lua
+++ load it.lua
@@ -1,2 +1,1 @@
-loadstring())()
---i didnt get link for main.lua.. oops!
+loadstring(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/softbf395/prime-boss/refs/heads/main/main.lua"))()("user")
