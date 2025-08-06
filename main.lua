d353ea8c9db2dd5c372581d6f55f8a5b4122c4eb
softbf395 <130502864+softbf395@users.noreply.github.com>
2025-08-05 19:21:33 -0500

    Update main.lua

--- main.lua
+++ main.lua
@@ -6,9 +6,9 @@ local pyramidMeshID = "rbxassetid://4712590845"
 local version=0.1
 local isWhitelist=version<1
 local whitelisted={
-"Aedaniss7",
-  "Greysoniss5",
-  "leonardomigueltest"
+"Aedaniss7", --me
+  "Greysoniss5", --my brother
+  "leonardomigueltest" --friend
 }
 function WhitelistFunc(pname)
   if table.find(whitelisted, pname) or isWhitelist==false then
